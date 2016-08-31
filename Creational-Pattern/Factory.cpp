1fff7d77eca1e3e53a53ad88dc69157602369866
Tu Le Phu Hoang <tupl@uci.edu>
2016-08-30 23:43:06 -0700

    finished Factory

--- Creational-Pattern/Factory.cpp
+++ Creational-Pattern/Factory.cpp
@@ -1,1 +1,40 @@
-:q
+#include <iostream>
+#include <string>
+
+class Character {
+public:
+    virtual ~Character() {};
+    virtual void sayHello() const = 0;
+};
+
+class Human: public Character {
+public:
+    ~Human () {}
+    void sayHello() const {
+        std::cout << "I'm a human" << std::endl;
+    }
+};
+
+class Evil: public Character {
+public:
+    ~Evil () {}
+    void sayHello() const {
+        std::cout << "I'm an evil" << std::endl;
+    }
+};
+
+class CharacterFactory {
+private:
+    CharacterFactory() {}
+public:
+    static Character* create(const std::string& desc) {
+        if (desc == "human") return new Human;
+        if (desc == "evil") return new Evil;
+    }
+};
+
+int main() {
+    (CharacterFactory::create("human"))->sayHello();
+    (CharacterFactory::create("evil"))->sayHello();
+    return 0;
+}
