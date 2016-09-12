1da7520e310297b9e543696543cb04e51e70e89c
Tu Le Phu Hoang <tupl@uci.edu>
2016-09-12 15:05:02 -0700

    finished jigsaw.cpp

---
+++ Object-Oriented-Practice/jigsaw.cpp
@@ -0,0 +1,13 @@
+/*
++ Idea:
+Implement a jigsaw puzzple with some functions
+
+- Each piece has four edge
+- Each edge has three types: inner, outer, flat
+- orientation of puzzle: 0, 90, 180, 270
+
+- First put all puzzle into available puzzles
+- Neighbor for each puzzle.
+- Three queue: done, execute, available
+
+*/
